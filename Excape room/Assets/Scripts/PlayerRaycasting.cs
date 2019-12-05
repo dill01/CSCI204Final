8bec12dc2fbf508d0045eeafe414c150a2dd3858
Hultman-JM <57850244+Hultman-JM@users.noreply.github.com>
2019-12-04 23:14:37 -0700

    Room 1 Attempt at interactable (raycasting)

--- Excape room/Assets/Scripts/PlayerRaycasting.cs
+++ Excape room/Assets/Scripts/PlayerRaycasting.cs
@@ -15,36 +15,48 @@ public class PlayerRaycasting : MonoBehaviour
     void Start()
     {
         test = SceneManager.GetActiveScene();
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.magenta);
-        if(Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distanceToSee))
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distanceToSee))
         {
-            if(whatIHit.collider.gameObject.name == "Correct")
-            {
-                Debug.Log("I touched the thing");
-                if (Input.GetKeyDown(KeyCode.E))
+
+            if (test.name.Equals("Room One"))
+            {   
+                
+                RoomOneGame hmm = Gamecontroller.GetComponent<RoomOneGame>();             
+                if (whatIHit.collider.gameObject.name == "CellLockPanel" && Input.GetKeyDown(KeyCode.E))
                 {
-                    Debug.Log("You Killed the thing");
-                    whatIHit.collider.gameObject.SetActive(false);
-                    if(test.name.Equals("Room One"))
+                    Debug.Log("Cell Lock Panel");
+                    hmm.CellLockPanelFlag = true;
+                    hmm.progress();
+                }
+                
+                
+            }
+            else if (test.name.Equals("Room Two"))
+            {
+                Debug.Log("um wtf jim");                        
+                RoomTwoGame hmm = Gamecontroller.GetComponent<RoomTwoGame>();
+                hmm.progress();
+                if (whatIHit.collider.gameObject.name == "Correct")
+                {
+                    Debug.Log("I touched the thing");
+                    if (Input.GetKeyDown(KeyCode.E))
                     {
+                        Debug.Log("You Killed the thing");
+                        whatIHit.collider.gameObject.SetActive(false);
 
-                    }
-                    else if(test.name.Equals("Room Two"))
-                    {
-                        Debug.Log("um wtf jim");
-                        RoomTwoGame hmm = Gamecontroller.GetComponent<RoomTwoGame>();
-                        hmm.progress();
 
-                        
                     }
                 }
+
             }
+            
         }
     }
 }
---
+++ Excape room/Assets/Scripts/RoomOneGame.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomOneGame : MonoBehaviour
+{
+    private int progression;
+    public bool CellLockPanelFlag;
+
+    GameObject CellLockPanel;
+    GameObject CellDoor;
+    Animator CellLockPanelAnim, CellDoorAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CellLockPanelAnim = CellLockPanel.GetComponent<Animator>();
+        CellDoorAnim = CellDoor.GetComponent<Animator>();
+        CellLockPanelFlag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CellLockPanelFlag == true)
+        {
+            CellLockPanelAnim.SetBool("Slide", true);
+        }
+    }
+
+    public void setFlag(bool inFlag)
+    {
+        inFlag = true;
+    }
+
+    public void progress()
+    {
+        Debug.Log("I added a number");
+        progression++;
+        Debug.Log("Progesstion is now " + progression);
+        int hmm = progression;
+        if (hmm == 1)
+        {
+            Debug.Log("wtf dude");
+
+
+        }
+        else if (hmm == 2)
+        {
+
+        }
+        else if (hmm == 3)
+        {
+
+        }
+        else if (hmm == 4)
+        {
+
+        }
+
+    }
+
+    public int Getprogression()
+    {
+        return progression;
+    }
+
+}
+

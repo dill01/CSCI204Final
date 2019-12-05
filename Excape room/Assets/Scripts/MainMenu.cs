8b677ca7cf6514353b95b98b121ce0cdff6ef99b
dill01 <dillon.prescott@gmail.com>
2019-12-04 19:11:38 -0700

    Added plater controller

---
+++ Excape room/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+    public void RoomOne()
+    {
+        SceneManager.LoadScene("Room One");
+    }
+    public void RoomTwo()
+    {
+        SceneManager.LoadScene("Room Two");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}

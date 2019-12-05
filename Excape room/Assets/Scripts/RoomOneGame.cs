a3d9b1f6e6a559cb404636b099d6e04fdd060566
Hultman-JM <57850244+Hultman-JM@users.noreply.github.com>
2019-12-05 08:40:05 -0700

    Room 1 Interactables Cell room working

--- Excape room/Assets/Scripts/RoomOneGame.cs
+++ Excape room/Assets/Scripts/RoomOneGame.cs
@@ -5,31 +5,71 @@ using UnityEngine;
 public class RoomOneGame : MonoBehaviour
 {
     private int progression;
-    public bool CellLockPanelFlag;
-
-    GameObject CellLockPanel;
-    GameObject CellDoor;
+    //public bool CellLockPanelFlag = false;
+    public GameObject WireOneBrokeTop;
+    public GameObject WireOneBrokeBottom;
+    public GameObject WireTwoBrokeTop;
+    public GameObject WireTwoBrokeBottom;
+    public GameObject WireThreeBrokeTop;
+    public GameObject WireThreeBrokeBottom;
+    public GameObject WireFourBrokeTop;
+    public GameObject WireFourBrokeBottom;
+    public GameObject CellLockPanel;
+    public GameObject CellDoor;
     Animator CellLockPanelAnim, CellDoorAnim;
     // Start is called before the first frame update
     void Start()
     {
         CellLockPanelAnim = CellLockPanel.GetComponent<Animator>();
         CellDoorAnim = CellDoor.GetComponent<Animator>();
-        CellLockPanelFlag = false;
+        CellLockPanelAnim.SetBool("Slide", false);
+        CellDoorAnim.SetBool("Slide", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (CellLockPanelFlag == true)
+        
+    }
+
+    public void interact(string inString)
+    {
+        if (inString == "CellLockPanel")
         {
+            Debug.Log("animation started");
             CellLockPanelAnim.SetBool("Slide", true);
+        }
+        else if (inString == "Wire 1")
+        {
+            Debug.Log("wire 1 is wrong");
+            WireOneBrokeTop.SetActive(true);
+            WireOneBrokeBottom.SetActive(true);
+        }
+        else if (inString == "Wire 2")
+        {
+            Debug.Log("wire 2 is wrong");
+            WireTwoBrokeTop.SetActive(true);
+            WireTwoBrokeBottom.SetActive(true);
+        }
+        else if (inString == "Wire 3")
+        {
+            Debug.Log("wire 3 is correct");
+            WireThreeBrokeTop.SetActive(true);
+            WireThreeBrokeBottom.SetActive(true);
+            CellDoorAnim.SetBool("Slide", true);
+        }
+        else if (inString == "Wire 4")
+        {
+            Debug.Log("wire 4 is wrong");
+            WireFourBrokeTop.SetActive(true);
+            WireFourBrokeBottom.SetActive(true);
         }
     }
 
     public void setFlag(bool inFlag)
     {
         inFlag = true;
+        
     }
 
     public void progress()
